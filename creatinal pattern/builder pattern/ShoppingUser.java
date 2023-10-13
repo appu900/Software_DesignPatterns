10e4912e9c9c4a4cec6d4e0bba9f836709d8c17b
pabitra <89703515+appu900@users.noreply.github.com>
2023-10-13 23:51:51 +0530

    setup

---
+++ creatinal pattern/builder pattern/ShoppingUser.java
@@ -0,0 +1,25 @@
+
+// basically  builder pattern reduce the complexity of creating complex object
+// some cases we have to use multiple constructer for create a instance of a class
+
+// problem statement
+
+// “Happy Shopping Store” is an online shopping system.
+//  There when a registered customer buys an item from the store it
+//  needs to provide five attributes as first name, last name, age, phone, and address 
+//  as the delivery details in order to deliver the item to the particular customer. 
+//  And here we want only the first name and the last name of the registered customer and the rest 
+//  other fields are optional. Because system already have the delivery details of all 
+//  the registered customers. So let’s solve this real world problem
+
+
+
+
+
+
+
+class ShoppingUser {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
