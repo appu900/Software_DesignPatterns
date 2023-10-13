d17491ad5aa47fd9b3a264f645ba4585ac75af46
pabitra <89703515+appu900@users.noreply.github.com>
2023-10-14 00:13:34 +0530

    builder shopping user problem

--- creatinal pattern/builder pattern/ShoppingUser.java
+++ creatinal pattern/builder pattern/ShoppingUser.java
@@ -4,7 +4,7 @@
 
 // problem statement
 
-// “Happy Shopping Store” is an online shopping system.
+// Happy Shopping Store is an online shopping system.
 //  There when a registered customer buys an item from the store it
 //  needs to provide five attributes as first name, last name, age, phone, and address 
 //  as the delivery details in order to deliver the item to the particular customer. 
@@ -12,14 +12,75 @@
 //  other fields are optional. Because system already have the delivery details of all 
 //  the registered customers. So let’s solve this real world problem
 
+class Customer {
+    private final String firstName;
+    private final String lastName;
+    private final String address;
+    private final String feedback;
+    private final String phoneNumber;
 
+    public Customer(Builder builder) {
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.address = builder.address;
+        this.feedback = builder.feedback;
+        this.phoneNumber = builder.phoneNumber;
+    }
 
+    static class Builder {
+        private String firstName;
+        private String lastName;
+        private String address;
+        private String feedback;
+        private String phoneNumber;
 
+        public Builder firstName(String firstname) {
+            this.firstName = firstname;
+            return this;
+        }
 
+        public Builder lastName(String lastname) {
+            this.lastName = lastname;
+            return this;
+        }
 
+        public Builder address(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder feedback(String feedback) {
+            this.feedback = feedback;
+            return this;
+        }
+
+        public Builder phoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public Customer build() {
+            return new Customer(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Customer [address=" + address + ", feedback=" + feedback + ", firstName=" + firstName + ", lastName="
+                + lastName + ", phoneNumber=" + phoneNumber + "]";
+    }
+
+}
 
 class ShoppingUser {
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+        Customer.Builder builder = new Customer.Builder();
+
+        Customer c1 = builder.firstName("Raj").lastName("Kumar").build();
+        Customer c2 = builder.firstName("pabitra").lastName("dakua").feedback("very good").address("balasore").build();
+
+        System.out.println(c1);
+        System.out.println(c2);
     }
 }
